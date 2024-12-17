3781a535f43323f85fe3571c639cd7baf35ec597
venkatk-git <venkatkumar.profiles@gmail.com>
2024-12-17 21:58:22 +0530

    api(db): Made Mongodb connection

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 import helmet from 'helmet';
 import session from 'express-session';
 import passport from 'passport';
@@ -7,12 +9,26 @@ import authRouter from './routes/auth.router';
 
 const app = express();
 
+// Connect to Mongodb
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('DB CONNECTED');
+  })
+  .catch((e: Error) => {
+    console.error(e.message);
+  });
+
+/**
+ ** helmet() is a middleware to enhance the of the application security by
+ ** setting various HTTP headers to protect against common vulnerabilities.
+ **/
 app.use(helmet());
 
-/*
-Session initialization using cookie-session 
- for handling the incomming session from passport
-*/
+/**
+ ** Session initialization using express-session
+ ** for handling the incoming session data from Passport.
+ **/
 app.use(
   session({
     name: 'sid',
