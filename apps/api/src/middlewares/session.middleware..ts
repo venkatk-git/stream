d75948d8b52b34aa7284056a3a049929e635a255
venkatk-git <venkatkumar.profiles@gmail.com>
2025-02-17 19:26:22 +0530

    api: tring fixing

--- apps/api/src/middlewares/session.middleware..ts
+++ apps/api/src/middlewares/session.middleware..ts
@@ -1,22 +1,22 @@
-import session from "express-session";
+import session from 'express-session';
 // import { redisClient } from "../redis";
 // import { RedisStore } from "connect-redis";
 
 // const store = new RedisStore({
-    // redisClient,
+// redisClient,
 // });
 
 const sessionMiddleware = session({
-    name: "session_id",
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET || "good day",
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24, // 1 day
-        secure: false,
-        httpOnly: true,
-        sameSite: "lax",
-    },
+  name: 'session_id',
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET || 'good day',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+    secure: false,
+    httpOnly: true,
+    sameSite: 'none',
+  },
 });
 
 export { sessionMiddleware };
