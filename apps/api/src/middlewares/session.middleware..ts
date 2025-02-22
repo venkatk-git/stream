f1235a996f22dffa487872272e0781c433237444
Venkat <119146167+venkatk-git@users.noreply.github.com>
2025-02-22 20:02:41 +0530

    api: update on session settings

--- apps/api/src/middlewares/session.middleware..ts
+++ apps/api/src/middlewares/session.middleware..ts
@@ -16,7 +16,7 @@ const sessionMiddleware = session({
     maxAge: 1000 * 60 * 60 * 24, // 1 day
     secure: false,
     httpOnly: true,
-    sameSite: 'lax',
+    sameSite: 'none',
     domain: process.env.CLIENT_ORIGIN,
   },
 });
