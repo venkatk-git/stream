b91f65867962791b90f4172ce8e1100786c0b2de
Venkat <119146167+venkatk-git@users.noreply.github.com>
2025-02-26 20:57:34 +0530

    app: update socket.ts

--- apps/web/src/socket.ts
+++ apps/web/src/socket.ts
@@ -1,10 +1,9 @@
-// ts-ignore
 import io from 'socket.io-client';
 import { DEPLOYED_API_BASE_URL } from './lib/constants';
 
 const socket = io(DEPLOYED_API_BASE_URL, {
   autoConnect: true,
   withCredentials: true,
-});
+} as any); // This is fine 
 
 export default socket;
